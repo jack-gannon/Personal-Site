ccda643105339cd7c581e942aff0a276fe3f0150
Jack Gannon <jackmakesart@gmail.com>
2020-01-09 21:42:42 -0700

    added color js variables

---
+++ src/utils/colors.js
@@ -0,0 +1,93 @@
+//   /* Primary */
+//   --primary: #56d6d6;
+//   --primary-darkest: #045959;
+//   --primary-darker: #269999;
+//   --primary-medium: #7fe7e7;
+//   --primary-light: #baf7f7;
+//   --primary-lightest: #e5ffff;
+
+//   /* Secondary */
+//   --secondary: #f26e6b;
+//   --secondary-darkest: #590a09;
+//   --secondary-darker: #992926;
+//   --secondary-medium: #e5817e;
+//   --secondary-light: #f7bbba;
+//   --secondary-lightest: #ffe6e6;
+
+//   /* Tertiary */
+//   --tertiary: #143351;
+//   --tertiary-darkest: #093259;
+//   --tertiary-darker: #266099;
+//   --tertiary-medium: #5ca2e5;
+//   --tertiary-light: #bad9f7;
+//   --tertiary-lightest: #e5f3ff;
+
+//   /* Quaternary */
+//   --quaternary: #efce58;
+//   --quaternary-darkest: #594809;
+//   --quaternary-darker: #997f26;
+//   --quaternary-medium: #e5ce7e;
+//   --quaternary-light: #f7eaba;
+//   --quaternary-lightest: #fff9e5;
+
+//   /* --------- Gray Scale -------- */
+//   --gray-10: #fafbfc;
+//   --gray-20: #eef2f4;
+//   --gray-30: #d7e0e5;
+//   --gray-40: #b9c6ce;
+//   --gray-50: #99a7af;
+//   --gray-60: #757f85;
+//   --gray-70: #4f5559;
+//   --gray-80: #363a3d;
+//   --gray-90: #222527;
+
+//   /* ---------- Shadows ---------- */
+
+//   --shadow-1: rgba(111, 124, 128, 0.15);
+//   --shadow-2: rgba(0, 0, 0, 0.35);
+//   --shadow-primary: rgba(104, 85, 156, 0.65);
+
+//   /* ---------- Gradients ---------- */
+//   --gradient-primary: linear-gradient(#68559c, #563d99);
+// }
+
+export const colors = {
+  // Primary
+  primary: "#56d6d6",
+  primaryDarkest: "#045959",
+  primaryDarker: "#269999",
+  primaryMedium: "#7fe7e7",
+  primaryLight: "#baf7f7",
+  primaryLightest: "#e5ffff",
+  // Secondary
+  secondary: "#f26e6b",
+  secondaryDarkest: "#590a09",
+  secondaryDarker: "#992926",
+  secondaryMedium: "#e5817e",
+  secondaryLight: "#f7bbba",
+  secondaryLightest: "ffe6e6",
+  // Tertiary
+  tertiary: "#143351",
+  tertiaryDarkest: "#093259",
+  tertiaryDarker: "#266099",
+  tertiaryMedium: "#5ca2e5",
+  tertiaryLight: "#bad9f7",
+  tertiaryLightest: "e5f3ff",
+  // Quaternary
+  quaternary: "#efce58",
+  quaternaryDarkest: "#594809",
+  quaternaryDarker: "#997f26",
+  quaternaryMedium: "#e5ce7e",
+  quaternaryLight: "#f7eaba",
+  quaternaryLightest: "fff9e5",
+  // Grayscale
+  gray10: "#fafbfc",
+  gray20: "#eef2f4",
+  gray30: "#d7e0e5",
+  gray40: "#b9c6ce",
+  gray50: "#99a7af",
+  gray60: "#757f85",
+  gray70: "#4f5559",
+  gray80: "#363a3d",
+  gray90: "#222527",
+}
