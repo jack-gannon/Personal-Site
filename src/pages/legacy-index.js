f6d098694e843d08091a3937f246c89bd0fd5d26
Jack Gannon <jackmakesart@gmail.com>
2020-02-11 19:12:40 -0700

    temporarily removed component that was causing query error

--- src/pages/legacy-index.js
+++ src/pages/legacy-index.js
@@ -24,7 +24,7 @@ class IndexPage extends React.Component {
           />
           <FeaturedProjects />
           <Services />
-          <FromTheBlog />
+          {/* <FromTheBlog /> */}
         </Layout>
       </>
     )
