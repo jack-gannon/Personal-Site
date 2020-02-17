dc00e99189b94f554b0e7c1098b55b0fa66a3f85
Jack Gannon <jackmakesart@gmail.com>
2020-02-17 15:43:39 -0700

    testing multiple portfolio paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,10 +10,10 @@ module.exports = {
     },
   },
   plugins: [
-    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-feed-mdx`,
