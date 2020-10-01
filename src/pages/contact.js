522ccb07adb5e3906d628aef563f03659fd94940
jack-gannon <jackgannondesign@gmail.com>
2020-10-01 16:10:04 -0700

    Revert "added custom meta descriptions for all pages"
    This reverts commit 6b2533412b5f99bd99538013228af7502995e18e.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,10 +14,7 @@ class Contact extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="Contact"
-          description="Do you want to contact Jack Gannon, or just stay connected? Follow him on social media, or use the contact form for project and employment inquiries. For a more accurate response, please provide a detailed description of your design or web development project, along with your contact information."
-        />
+        <SEO title="Contact" />
         <ContactContainer>
           <StayConnected />
           <GetInTouch />
